c35b0c80b6c469c08094e37d8950c7e5fdb8dd03
Atzen2 <Atzen2@web.de>
2016-04-09 14:28:16 +0200

    dailybuild_09042016
    - finished SchM

--- imports/startup/startupServer.js
+++ imports/startup/startupServer.js
@@ -1,12 +1,11 @@
 import '../api/KrakenApi.js';
 import '../api/serverCallbacks.js'
-import '../tools/TaskM.js';
+import '../tools/tasks.js';
 
 import '../api/krakenApi__.js';
 
 
-
 Meteor.startup(() => {
   // code to run on server at startup
-  SyncedCron.start();
+  // SyncedCron.start();
 });
--- imports/tools/InstHandler.js
+++ imports/tools/InstHandler.js
@@ -1,35 +1,32 @@
+/**
+ * @description:
+ * Saves instances into an array and manages them
+ *
+ * - no dependencies
+ * 
+ * @author Atzen
+ * @version 1.0
+ * 
+ * CHANGES:
+ * 12-Apr-2016 : Initial version
+ */
 
+
+/***********************************************************************
+  Private Static Variable
+ ***********************************************************************/
 
 /***********************************************************************
   Public Static Variable
  ***********************************************************************/
 
-// ClassName.Variable = 'Value';
-
-
-/***********************************************************************
-  private Static Variable
- ***********************************************************************/
-
-// var variable = 'Value';
-
-
-/***********************************************************************
-  Public Static Function
- ***********************************************************************/
-
-// ClassName.function = function(param){
-//   return 'Value';
-// }
-
-
 /***********************************************************************
   Private Static Function
  ***********************************************************************/
 
-// var variable = function(param){
-//   return 'Value';
-// }
+/***********************************************************************
+  Public Static Function
+ ***********************************************************************/
 
 /***********************************************************************
   Class
@@ -41,36 +38,38 @@ export function InstHandler() {
     Private Instance Variable
    ***********************************************************************/
   
+  /**
+   * internal array that holds all objects
+   * @type {Array}
+   */
   var objArray = new Array();
 
 
   /***********************************************************************
     Public Instance Variable
    ***********************************************************************/
-   
-   // this.Variable = 'Value'; 
-
 
   /***********************************************************************
     Private Instance Function
    ***********************************************************************/
 
-  // var functionName = function(param) {
-  //   return 'Value';
-  // }
-
-}  
-
   /***********************************************************************
     Public Instance Function
    ***********************************************************************/
 
-
-  this.addObj = function(obj, id) {
+  /**
+   * adds an object to the internal array
+   * @param {string} id  id which will be attached to the object
+   * @param {Object} obj object which should be stored
+   * @return {bool}      true if successful
+   */
+  this.addObj = function(id, obj) {
     var added = false;
 
 
+    /* saves obj in a free element space or overrides it (if same id was found) */
     objArray.every(function(element, index, array) {
+    	var idx = 0;
       if(index < array.length){
         if (element === 'undefined' || array[index]['id'] === id) {
           array[index] = new Object();
@@ -82,10 +81,11 @@ export function InstHandler() {
       }
     });
 
+    /* if no space is available, push obj to the end */
     if (added === false) {
       objArray[objArray.length] = new Object();
-      objArray[index]['obj'] = Object.assign({}, obj);
-      objArray[index]['id'] = id;
+      objArray[objArray.length - 1]['obj'] = Object.assign({}, obj);
+      objArray[objArray.length - 1]['id'] = id;
       return true;
     }
 
@@ -93,15 +93,23 @@ export function InstHandler() {
   }
 
 
+  /**
+   * removes an element from the internal array
+   * @param  {string} id id of object which should be removed
+   * @return {bool}      true if successful
+   */
   this.removeObj = function(id) {
     var found = false;
     
+
+    /* get element index of object */
     var idx = objArray.findIndex(function(element, index, array){
       if(element['id'] === id)
         found = true;
         return true;
     });
 
+    /* remove element */
     if(found === true){
       objArray.delete(idx);
       return true;
@@ -111,21 +119,38 @@ export function InstHandler() {
   }
 
 
+  /**
+   * returns an object
+   * @param  {string} id id of object which should be returned
+   * @return {Object}    object which should be returned (returns 'undefined' if not found)
+   */
   this.getObj = function(id){
     var found = false;
     
+    /* get element index of object */
     var idx = objArray.findIndex(function(element, index, array){
-      if(element['id'] === id)
+      if(element['id'] === id){
         found = true;
         return true;
+      }
     });
 
+    /* return object */
     if(found === true){
-      return objArray[idx];
+      return objArray[idx]['obj'];
     }
 
     return 'undefined';
   }
 
+
+  /**
+   * clears the internal array (deletes all elements)
+   * @return {bool} true if successful
+   */
+  this.clear = function(){
+  	objArray = new Object();
+  	return true;
+  }
 }
 
