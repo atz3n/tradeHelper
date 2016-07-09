d212ff27cd52d438040297d0059ba68f1837da7c
Atzen2 <Atzen2@web.de>
2016-07-09 13:58:01 +0200

    v1.1 IExchange
    - added getPositions interface

--- server/methods/serverCalls.js
+++ server/methods/serverCalls.js
@@ -1,10 +1,10 @@
-import{ExTestData} from '../trading/exchanges/ExTestData.js';
+import { ExTestData } from '../trading/exchanges/ExTestData.js';
 
 
 var exchange = {};
 
 
-var bFunc = function(instInfo, errObject){
+var bFunc = function(instInfo, errObject) {
   console.log('bought');
   console.log(instInfo);
   console.log(errObject);
@@ -12,7 +12,7 @@ var bFunc = function(instInfo, errObject){
   console.log('tPrice: ' + exchange.getTradePrice().result);
 }
 
-var sFunc = function(instInfo, errObject){
+var sFunc = function(instInfo, errObject) {
   console.log('sold');
   console.log(instInfo);
   console.log(errObject);
@@ -25,69 +25,69 @@ var sFunc = function(instInfo, errObject){
 
 
 Meteor.methods({
-  create: function(){
+  create: function() {
     exchange = new ExTestData();
     console.log('create');
   },
 
-  setConfig: function(config){
+  setConfig: function(config) {
     console.log('setConfig');
     exchange.setBoughtNotifyFunc(bFunc);
     exchange.setSoldNotifyFunc(sFunc);
     return exchange.setConfig(config);
   },
 
-  getConfig: function(){
+  getConfig: function() {
     console.log('getConfig');
     return exchange.getConfig();
   },
 
-  getInfo: function(){
+  getInfo: function() {
     console.log('getInfo')
     return exchange.getInfo();
   },
 
-  getPairUnits: function(){
+  getPairUnits: function() {
     console.log('getPairUnits');
     return exchange.getPairUnits();
   },
 
-  getVolume: function(){
+  getVolume: function() {
     console.log('getVolume')
     return exchange.getVolume();
   },
 
-  getPrice: function(){
+  getPrice: function() {
     console.log('getPrice')
     return exchange.getPrice();
   },
 
-  getTradePrice: function(){
+  getTradePrice: function() {
     console.log('getTradePrice')
     return exchange.getTradePrice();
   },
 
-  update: function(){
+  update: function() {
     console.log('update');
     return exchange.update();
   },
 
-  sell: function(){
+  sell: function() {
     console.log('sell');
     return exchange.sell('long');
   },
 
-  buy: function(){
+  buy: function() {
     console.log('buy');
     return exchange.buy('long');
   },
 
-  stopTrade: function(){
+  stopTrade: function() {
     console.log('stopTrade');
     return exchange.stopTrade();
   },
 
-  getInstInfo: function(){
+  getInstInfo: function() {
     console.log('getInstInfo');
     return exchange.getInstInfo();
   },
@@ -95,5 +95,10 @@ Meteor.methods({
   getTradePairInfos: function() {
     console.log('getTradePairInfos')
     return ExTestData.getTradePairInfos();
+  },
+
+  getPositions: function() {
+    console.log('getPositions')
+    return exchange.getPositions();
   }
 });
